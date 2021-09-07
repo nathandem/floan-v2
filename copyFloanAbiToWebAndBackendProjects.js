9bbe8ccdfc88dbf8935491e9f42e961039f4027c
Nathan DM <nathan.demaestri@gmail.com>
2021-09-07 17:10:28 +0200

    chore: remove yarn workspace, use simple monorepo instead
    Issues with yarn workspaces:
    - centralize dependencies at the top level of the project,
      could create issue. Prefer to have each project have its
      own dependencies.
    - just prefer to have each project totally seperated, especially
      if I add a non-JS project later on
    - need to rely on yarn

--- copyFloanAbiToWebAndBackendProjects.js
+++ copyFloanAbiToWebAndBackendProjects.js
@@ -1,11 +1,6 @@
 /*
  * Create a copy of Floan hardhat generated ABI in `/abis`.
  * `/abis` is the directory in which floan-web and floan-backend read Floan ABI.
- * 
- * IMPORTANT:
- * This script should be called from the root of the project, else the paths won't
- * resolve properly. Best way to run this script is: `yarn run contract:compile`
- * from the root of the project.
  */
 
 const fs = require('fs');
@@ -13,10 +8,10 @@ const fs = require('fs');
 
 // constants
 
-const HARDHAT_ARTIFACT_DEFAULT_PATH_FOR_FLOAN_CONTRACT = './packages/floan-contract/artifacts/contracts/Floan.sol/Floan.json';
-const FLOAN_BACKEND_ABIS_PATH = './packages/floan-backend/abis/Floan.json';
+const HARDHAT_ARTIFACT_DEFAULT_PATH_FOR_FLOAN_CONTRACT = './floan-contract/artifacts/contracts/Floan.sol/Floan.json';
+const FLOAN_BACKEND_ABIS_PATH = './floan-backend/abis/Floan.json';
 // note: by default create-react-app doesn't allow import outside of `src`, let's stick with that
-const FLOAN_WEB_ABIS_PATH = './packages/floan-web/src/abis/Floan.json';
+const FLOAN_WEB_ABIS_PATH = './floan-web/src/abis/Floan.json';
 
 
 // logic
