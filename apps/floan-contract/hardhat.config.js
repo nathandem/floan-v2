aa95f4fc7a99dc6788926710623a42e47054a480
Nathan DM <nathan.demaestri@gmail.com>
2021-09-10 14:22:34 +0200

    fix: set defaults to env vars to avoid crashing hardhat

--- apps/floan-contract/hardhat.config.js
+++ apps/floan-contract/hardhat.config.js
@@ -2,8 +2,8 @@ require("@nomiclabs/hardhat-waffle");
 
 
 // load private data to use in deployment
-const ALCHEMY_KOVAN_API_KEY = process.env.ALCHEMY_KOVAN_API_KEY;
-const DEPLOYMENT_ACCOUNT = process.env.DEPLOYMENT_ACCOUNT;
+const ALCHEMY_KOVAN_API_KEY = process.env.ALCHEMY_KOVAN_API_KEY || 'dummy';
+const DEPLOYMENT_PK = process.env.DEPLOYMENT_PK || '0000000000';
 
 // https://hardhat.org/config/
 /**
@@ -26,10 +26,12 @@ module.exports = {
             // gas: 1000000,
             // gasPrice: 1000,
         },
+        // LEAVE THE OUTSIDE NETWORKS COMMENTED BECAUSE THE CONF IS NOT LAZYLY LOADED
+        // AND CRASHES IF ENV VARS ARE NOT READY
         kovan: {
             url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_KOVAN_API_KEY}`,
             // accounts: { mnemonic: METAMASK_MNEMONICS },
-            accounts: [`0x${DEPLOYMENT_ACCOUNT}`],
+            accounts: [`0x${DEPLOYMENT_PK}`],
         },
         // ropsten: {
         //     url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}/`,
